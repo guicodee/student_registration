c51224b99767c6afe0cb247b64ddd33cab729b65
guicodee <guilhermedfgo12@gmail.com>
2026-02-24 20:19:49 -0300

    Register repositories and services

---
+++ Configuration/ServiceColletion.cs
@@ -0,0 +1,27 @@
+﻿using ConceptsDB.Data;
+using ConceptsDB.Repository;
+using ConceptsDB.Repository.Courses;
+using ConceptsDB.Repository.Matriculations;
+using ConceptsDB.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ConceptsDB.Configuration;
+
+public static class ServiceColletion
+{
+    public static IServiceCollection Services(this IServiceCollection services)
+    {
+        services.AddScoped<AppDbContext>();
+        
+        services.AddScoped<IStudentRepository, StudentRepository>();
+        services.AddScoped<StudentService>();
+        
+        services.AddScoped<ICourseRepository, CourseRepository>();
+        services.AddScoped<CourseService>();
+        
+        services.AddScoped<IMatriculationRepository, MatriculationRepository>();
+        services.AddScoped<MatriculationService>();
+
+        return services;
+    }
+}
