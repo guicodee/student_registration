384d2bf9ee2c4dbb6f5083e4d5a1af0640d98385
guicodee <guilhermedfgo12@gmail.com>
2026-02-24 20:15:46 -0300

    Implement MatriculationService

---
+++ Services/MatriculationService.cs
@@ -0,0 +1,55 @@
+﻿using ConceptsDB.Models;
+using ConceptsDB.Repository.Matriculations;
+
+namespace ConceptsDB.Services;
+
+public class MatriculationService
+{
+    private readonly IMatriculationRepository _matriculationRepository;
+
+    public MatriculationService(IMatriculationRepository matriculationRepository)
+    {
+        _matriculationRepository = matriculationRepository;
+    }
+
+    public async Task EnrollStudent(Matriculation matriculation)
+    {
+        var studentIsAlreadyEnrolled = await IsAlreadyEnrolled(
+            matriculation.StudentId, 
+            matriculation.CourseId
+        );
+
+        if (studentIsAlreadyEnrolled)
+        {
+            throw new Exception("Estudante já está matriculado nesse curso.");
+        }
+
+        await _matriculationRepository.EnrollStudent(matriculation);
+    }
+
+    public async Task UnenrollStudent(int idStudent, int idCourse)
+    {
+        var studentIsAlreadyEnrolled = await IsAlreadyEnrolled(idStudent, idCourse);
+
+        if (!studentIsAlreadyEnrolled)
+        {
+            throw new Exception("Estudante não está matriculado nesse curso.");
+        }
+
+        await _matriculationRepository.UnenrollStudent(idStudent, idCourse);
+    }
+
+    public async Task<bool> IsAlreadyEnrolled(int idStudent, int idCourse)
+    {
+        var enrolled = await _matriculationRepository.IsAlreadyEnrolled(idStudent, idCourse);
+
+        return enrolled;
+    }
+
+    public async Task<List<Matriculation>> GetMatriculationsOfStudent(int idStudent)
+    {
+        var matriculations = await _matriculationRepository.GetMatriculationsOfStudent(idStudent);
+
+        return matriculations;
+    }
+}
