8c59a86a73ddea8a562cc43bdb221896d57e9ffe
guicodee <guilhermedfgo12@gmail.com>
2026-02-26 12:42:14 -0300

    add safe integer parsing with null handling

---
+++ Utils/VerifyToNumberReceived.cs
@@ -0,0 +1,18 @@
+﻿namespace ConceptsDB.Utils;
+
+public class VerifyToNumberReceived
+{
+    public static int? ParseInt(string numberInput)
+    {
+        int number;
+        var isNumber = int.TryParse(numberInput, out number) && number > 0;
+
+        if (!isNumber)
+        {
+            Console.WriteLine("O ID passado não é do tipo número.");
+            return null;
+        }
+        
+        return number;
+    }
+}
