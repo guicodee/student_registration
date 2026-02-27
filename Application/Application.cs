82de7a66f88526c47a68ed08b4a9f3d8dc8ff9be
guicodee <guilhermedfgo12@gmail.com>
2026-02-27 14:01:45 -0300

    integrate application flow with menu

---
+++ Application/Application.cs
@@ -0,0 +1,19 @@
+﻿using ConceptsDB.Application.Menu;
+
+namespace ConceptsDB.Application;
+
+public class Application(RunMenu menu)
+{
+    public async Task Run()
+    {
+        bool running = true;
+
+        while (running)
+        {
+            menu.ShowMenu();
+            var option = menu.ReadOptions();
+
+            running = await menu.ExecuteAsync(option);
+        }
+    }    
+}
